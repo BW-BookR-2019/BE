c68384e24d8676ec049491df2fe647058caf527f
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2019-09-21 20:45:30 -0400

    :white_check_mark: Review model, done
    Review model, done

--- java-be/src/main/java/com/bookr/backend/models/Review.java
+++ java-be/src/main/java/com/bookr/backend/models/Review.java
@@ -1,6 +1,76 @@
 package com.bookr.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reviews")
 public class Review
 {
-  // blah blah
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long reviewid;
+
+    @Column(nullable = false)
+    private String review;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid",
+                nullable = false)
+    @JsonIgnoreProperties({"review", "hibernateLazyInitializer"})
+    private User user;
+
+    private int rating;
+
+    public Review()
+    {
+    }
+
+    public Review(String review, User user, int rating)
+    {
+        this.review = review;
+        this.user = user;
+        this.rating = rating;
+    }
+
+    public long getReviewid()
+    {
+        return reviewid;
+    }
+
+    public void setReviewid(long reviewid)
+    {
+        this.reviewid = reviewid;
+    }
+
+    public String getReview()
+    {
+        return review;
+    }
+
+    public void setReview(String review)
+    {
+        this.review = review;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    public int getRating()
+    {
+        return rating;
+    }
+
+    public void setRating(int rating)
+    {
+        this.rating = rating;
+    }
 }
