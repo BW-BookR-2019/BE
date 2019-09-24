a32c0a6366c313bfa2a8479c2137c42e8242a6a8
RealWillBrooks <wbrooks107@gmail.com>
2019-09-23 22:07:00 -0500

    Updates, no more errors within my IntelliJ

--- java-be/src/main/java/com/bookr/backend/controllers/BookController.java
+++ java-be/src/main/java/com/bookr/backend/controllers/BookController.java
@@ -1,8 +1,8 @@
 package com.bookr.backend.controllers;
 
-import com.bookr.backend.models.book;
-import com.bookr.backend.services.bookservice;
-import com.bookr.backend.models.errordetail;
+import com.bookr.backend.models.Book;
+import com.bookr.backend.services.BookService;
+import com.bookr.backend.models.ErrorDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +17,9 @@ import io.swagger.annotations.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/books")
-public class BookController
-{
+public class BookController {
     private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
@@ -43,7 +42,7 @@ public class BookController
                     paramType = "query",
                     value = "Sorting criteria in the format: property(,asc|desc). "
                             + "Default sort order is ascending. " + "Multiple sort criteria are supported.")})
-    @GetMapping(value ="/books", produces = {"application/json"})
+    @GetMapping(value = "/books", produces = {"application/json"})
     public ResponseEntity<?> findAllBooks(HttpServletRequest request,
                                           @PageableDefault(page = 0,
                                                   size = 3)
@@ -52,9 +51,9 @@ public class BookController
                 request.getRequestURI() + " accessed at warn level");
         List<Book> myBooks = bookService.findAll(pageable);
         return new ResponseEntity<>(myBooks, HttpStatus.OK);
-}
+    }
 
-    @ApiOperation(value="Deletes a Book Based on Id",
+    @ApiOperation(value = "Deletes a Book Based on Id",
             response = void.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200,
@@ -67,9 +66,9 @@ public class BookController
                     required = true,
                     example = "1")
             @PathVariable long bookid,
-            HttpServletRequest request)
-    {
+            HttpServletRequest request) {
         logger.info(request.getMethod() + " " + request.getRequestURI() + " accessed");
         bookService.delete(bookid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+}
--- java-be/src/main/java/com/bookr/backend/controllers/ReviewController.java
+++ java-be/src/main/java/com/bookr/backend/controllers/ReviewController.java
@@ -1,3 +1,28 @@
-package com.bookr.backend.controllers;
-
-import
+//package com.bookr.backend.controllers;
+//
+//import com.bookr.backend.models.review;
+//import com.bookr.backend.models.errordetail;
+//import com.bookr.backend.services.reviewservice;
+//
+//import io.swagger.annotations.*;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.web.PageableDefault;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/", )
+//public class ReviewController
+//{
+//    PostMapping(path="/reviews")
+//        public void addReviewV1(@RequestBody Review review){
+//
+//        }
+//}
